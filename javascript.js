2f30d6ce3ca3b17c11c79f49e2893c9fdb6fec51
Motato123456 <elliotyap00@gmail.com>
2024-07-02 09:37:33 +0800

    Completed 2nd part of tutorial

--- javascript.js
+++ javascript.js
@@ -1,9 +1,13 @@
+let input = prompt("Give a grid number:")
+
+
+
 const container = document.querySelector (".container");
 for (i=0; i<16; i++) {
-    var row = document.createElement("div");
+    const row = document.createElement("div");
     row.className = "row"
     for (j=0;j<16;j++) {
-        var cell = document.createElement ("div");
+        const cell = document.createElement ("div");
         cell.className = "cell"
         cell.addEventListener('mouseover', () => {
             cell.style.backgroundColor = "pink";
